75e0eeefbf1794c2181ff2328614d858df364ab6
Carla Bueno <cbuenosanudo@gmail.com>
2023-01-10 23:12:52 +0100

    Interaction 1&2

---
+++ JS_Basic_Algorithms/Interaction1.js
@@ -0,0 +1,12 @@
+let myFavoriteHero = "Hulk";
+
+let x = 50;
+
+const h = 5;
+y = 10;
+
+const z = h + y;
+console.log(z)
+
+
+
---
+++ JS_Basic_Algorithms/Interaction2.js
@@ -0,0 +1,20 @@
+const character = {name: 'Jack Sparrow', age: 25};
+
+const firstName = "Jon";
+const lastName = "Snow";
+const age = 24;
+console.log("Soy", firstName, lastName, ", tengo", age, "años y me gustan los lobos."
+)
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+const price1 = 19
+const price2 = 29
+console.log(price1 + price2)
+
+let globalBasePrice1 = 10000;
+const car1_1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2_1 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+let globalBasePrice2 = 20000;
+
+
