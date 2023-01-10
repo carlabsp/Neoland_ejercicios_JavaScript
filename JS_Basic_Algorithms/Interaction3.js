f5dcc97cb6d20d479d03ba957a17219813745c28
Carla Bueno <cbuenosanudo@gmail.com>
2023-01-10 23:39:55 +0100

    interaction3

---
+++ JS_Basic_Algorithms/Interaction3.js
@@ -0,0 +1,22 @@
+let x = 10;
+x *= 5;
+console.log(x)
+
+let y = 10;
+y /= 2;
+console.log(y)
+
+let z = 15;
+z /= 9;
+console.log(z)
+
+let p = 10;
+j = 5;
+o = p + j;
+console.log(o)
+
+let c = 10;
+m = 5;
+i = c *= m;
+console.log(i)
+
