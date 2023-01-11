feb05e81b5d2bbf8910fca0894f41f4c535cd0c9
Carla Bueno <cbuenosanudo@gmail.com>
2023-01-11 23:14:48 +0100

    Interaction4.js

---
+++ JS_Basic_Algorithms/Iteraction4.js
@@ -0,0 +1,28 @@
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+const avenger = avengers[0];
+console.log(avenger);
+
+const avengers2 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avengers2.splice(0, 1, "IRONMAN");
+console.log(avengers2);
+
+const avengers3 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers3.length);
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters); 
+const rickAndMortyCharacter = rickAndMortyCharacters[4];
+console.log(rickAndMortyCharacter) 
+
+const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters2.pop();
+console.log(rickAndMortyCharacters2);
+
+const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+
+
+
+
+
