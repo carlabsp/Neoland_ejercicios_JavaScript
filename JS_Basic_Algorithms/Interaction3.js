25a8626915d36794789ee162d60c482d507f1909
Carla Bueno <cbuenosanudo@gmail.com>
2023-01-15 19:21:11 +0100

    fixed

--- JS_Basic_Algorithms/Interaction3.js
+++ JS_Basic_Algorithms/Interaction3.js
@@ -1,25 +1,25 @@
-//1.1
+//3.1
 let x = 10;
 x *= 5;
 console.log(x);
 
-//1.2
+//3.2
 let y = 10;
 y /= 2;
 console.log(y);
 
-//1.3
+//3.3
 let z = 15;
 z /= 9;
 console.log(z);
 
-//1.4
+//3.4
 let p = 10;
 j = 5;
 o = p + j;
 console.log(o);
 
-//1.5
+//3.5
 let c = 10;
 m = 5;
 i = c *= m;
--- JS_Basic_Algorithms/Interaction4.js
+++ JS_Basic_Algorithms/Interaction4.js
@@ -1,25 +1,25 @@
-//1.1
+//4.1
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 const avenger = avengers[0];
 console.log(avenger);
 
-//1.2
+//4.2
 const avengers2 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 avengers2.splice(0, 1, "IRONMAN");
 console.log(avengers2);
 
-//1.3
+//4.3
 const avengers3 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 console.log(avengers3.length);
 
-//1.4
+//4.4
 const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
 rickAndMortyCharacters.push("Morty", "Summer");
 console.log(rickAndMortyCharacters);
 const rickAndMortyCharacter = rickAndMortyCharacters[4];
 console.log(rickAndMortyCharacter);
 
-//1.5
+//4.5
 const rickAndMortyCharacters2 = [
   "Rick",
   "Beth",
@@ -31,7 +31,7 @@ const rickAndMortyCharacters2 = [
 rickAndMortyCharacters2.pop();
 console.log(rickAndMortyCharacters2);
 
-//1.6
+//4.6
 const rickAndMortyCharacters3 = [
   "Rick",
   "Beth",
